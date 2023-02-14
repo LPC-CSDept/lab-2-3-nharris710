9ad2aa76be89a69e6c55f9fe176926b8af4b3ef0
nharris710 <noaharris248@gmail.com>
2023-02-13 23:31:50 -0800

    calculate salary

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+workhours = int(input('Enter work hours'))
+reg_hours = int(40)
+o_hours = int(10)
+reg_rate = float(18.25)
+o_rate = float(27.78)
+overtime_charge = ( o_hours * o_rate)
+regular_charge = (reg_hours * reg_rate)
+print('regular_charge: ',reg_hours * reg_rate)
+print('overtime_charge: ',o_hours * o_rate)
+total_wage = regular_charge + overtime_charge
+print('total_wage: ',regular_charge + overtime_charge)
