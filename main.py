f98d15d913d19af0fa6832a521b190e100fb7256
nharris710 <noaharris248@gmail.com>
2023-02-16 15:28:33 -0800

    Salary charge

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ def main():
     # Comlete your code here
     ##################################################
 
-    pass
-
-
-if __name__ == '__main__':
-    main()
-workhours = int(input('Enter work hours'))
+    workhours = int(input('Enter work hours'))
 reg_hours = int(40)
 o_hours = int(10)
 reg_rate = float(18.25)
@@ -19,3 +14,7 @@ print('regular_charge: ',reg_hours * reg_rate)
 print('overtime_charge: ',o_hours * o_rate)
 total_wage = regular_charge + overtime_charge
 print('total_wage: ',regular_charge + overtime_charge)
+pass
+
+if __name__ == '__main__':
+    main()
