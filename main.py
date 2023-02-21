7b9b65cb87660b07b3b214696be29bf922217c4c
nharris710 <noaharris248@gmail.com>
2023-02-20 18:46:34 -0800

    average number

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ def main():
     # Comlete your code here
     ##################################################
 
-    workhours = int(input('50 work hours'))
-reg_hours = int(40)
-o_hours = int(10)
-reg_rate = float(18.25)
-o_rate = float(27.78)
-overtime_charge = ( o_hours * o_rate)
-regular_charge = (reg_hours * reg_rate)
-print('regular_charge: ',reg_hours * reg_rate)
-print('overtime_charge: ',o_hours * o_rate)
-total_wage = regular_charge + overtime_charge
-print('total_wage: ',regular_charge + overtime_charge)
-pass
+    var1 = int(input('Enter your first number'))
+    var2 = int(input('Enter your second number '))
+    var3 = int(input('Enter your third number '))
+
+    total = var1 + var2 + var3
+    print(f'The summation is {total:>20}')
+
+    avg = total / 3
+    print(f'The summation is {avg:.2f}')
+
 
 if __name__ == '__main__':
     main()
